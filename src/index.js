35dfebfbe498055ff251ca588c86b014ddc457a2
nimetozk <nimet.ozakca@city.ac.uk>
2021-02-15 21:59:47 +0000

    update files

--- src/index.js
+++ src/index.js
@@ -8,24 +8,25 @@ import bankAccountRouter from "./routers/bank-account-router";
 import categoryRouter from "./routers/category-router";
 import taskRouter from "./routers/task-router";
 import authRouter from "./routers/auth-router";
+import cors from "cors";
 
-const startServer  = async()=>{
+const startServer = async () => {
+  await DbLoader();
 
- await DbLoader();
- 
- const app = express(); 
- app.use(bodyParser.json());
+  const app = express();
+  app.use(cors());
+  app.use(bodyParser.json());
 
- app.use("/api",userRouter);
- app.use('/api',transactionRouter);
- app.use('/api',bankAccountRouter);
- app.use('/api',categoryRouter);
- app.use('/api',taskRouter);
- app.use('/api',authRouter);
+  app.use("/api", userRouter);
+  app.use("/api", transactionRouter);
+  app.use("/api", bankAccountRouter);
+  app.use("/api", categoryRouter);
+  app.use("/api", taskRouter);
+  app.use("/api", authRouter);
 
-  app.listen(config.port, function(){
+  app.listen(config.port, function () {
     console.log(`Server started listening on ${config.port}`);
   });
-}
+};
 
 startServer();
